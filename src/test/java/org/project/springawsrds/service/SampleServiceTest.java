d78115830107a78838b91eac8faec173b3abab33
serviceappearance <sebyoug2647@gmail.com>
2024-06-21 11:47:58 +0900

    Test 코드 추가

---
+++ src/test/java/org/project/springawsrds/service/SampleServiceTest.java
@@ -0,0 +1,12 @@
+package org.project.springawsrds.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SampleServiceTest {
+
+  @Test
+  void getAllSample() {
+  }
+}
